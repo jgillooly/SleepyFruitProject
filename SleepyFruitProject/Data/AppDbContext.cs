a611c954ecf458a506dc26d388cd3931e21c294d
jgillooly <122410857+jgillooly@users.noreply.github.com>
2024-02-13 18:21:46 -0700

    Initial Database Setup and configuration

---
+++ SleepyFruitProject/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SleepyFruitProject.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+    }
+}
--- SleepyFruitProject/Program.cs
+++ SleepyFruitProject/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using SleepyFruitProject.Data;
+
 namespace SleepyFruitProject
 {
     public class Program
@@ -8,6 +11,9 @@ namespace SleepyFruitProject
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
+
+            builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 
             var app = builder.Build();
 
