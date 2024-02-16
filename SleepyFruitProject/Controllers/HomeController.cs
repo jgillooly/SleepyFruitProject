50d7d7d46405f39b993fa9b3473ed8330bcb22a2
Jacorey-creator <129239117+Jacorey-creator@users.noreply.github.com>
2024-02-15 19:44:34 -0700

    SIKE

--- SleepyFruitProject/Controllers/HomeController.cs
+++ SleepyFruitProject/Controllers/HomeController.cs
@@ -18,13 +18,40 @@ namespace SleepyFruitProject.Controllers
             return View();
         }
         [HttpGet]
-        public IActionResult End_Fake()
+        public IActionResult End_1()
         { 
             return View();
         }
         [HttpPost]
-        public IActionResult End_Fake(FinalGuess fg)
+        public IActionResult End_1(FinalGuess fg)
         {
+            //Custom validation
+            if (fg.Answer1 != 2)
+            {
+                ModelState.AddModelError("Answer1", "What do you take me for an idiot?");
+            }
+            if (!fg.Answer2.Equals("Pineapple", StringComparison.OrdinalIgnoreCase))
+            {
+                ModelState.AddModelError("Answer2", "Your time thanks you for the gains");
+            }
+            if (!fg.Answer3.Equals("declaration"))
+            {
+                ModelState.AddModelError("Answer3", "I can't belive you didn't get this 1");
+            }
+            if (!fg.Answer4.Equals("pie", StringComparison.OrdinalIgnoreCase))
+            {
+                ModelState.AddModelError("Answer4", "You deserve a worse time >:)");
+            }
+            if (!fg.Answer5.Equals("U"))
+            {
+                ModelState.AddModelError("Answer5", "C'mon the quiz isn't impossible!");
+            }
+            if (ModelState.IsValid)
+            {
+                //dal.AddMovie(m);
+                return RedirectToAction("GloatScreen", "Home");
+                //return View();
+            }
             return View();
         }
         public IActionResult Privacy()
@@ -35,7 +62,11 @@ namespace SleepyFruitProject.Controllers
 		{
 			return View();
 		}
-		public IActionResult yay()
+        public IActionResult GloatScreen()
+        {
+            return View();
+        }
+        public IActionResult yay()
 		{
 			return Redirect("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
 		}
